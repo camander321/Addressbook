cb6277a9c4771322cb5c0a6bfc34933aeeaab709
camander321 <chamburg321@gmail.com>
2018-02-09 10:24:26 -0800

    add css styling

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace AddressBook
             app.UseStaticFiles();
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Something went horribly wrong :0");
             });
         }
     }
