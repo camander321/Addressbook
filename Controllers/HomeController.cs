88b40fdd6f5e503c1bcc160bcefd16a8cb254585
camander321 <chamburg321@gmail.com>
2018-02-09 12:06:43 -0800

    add removal option for contacts

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,56 +5,70 @@ using System.Collections.Generic;
 
 namespace AddressBook.Controllers
 {
-    public class HomeController : Controller
-    {
+  public class HomeController : Controller
+  {
 
-        [HttpGet("/")]
-        public ActionResult Index()
-        {
-          return View("Index");
-        }
-        
-        [HttpPost("/")]
-        public ActionResult AddNew()
-        {
-          string name = Request.Form["name"];
-          if (name.Length > 0)
-          {
-            Address address = new Address(
-              Request.Form["address"],
-              Request.Form["city"],
-              Request.Form["state"],
-              Request.Form["zip"]
-            );
-            
-            Contact contact = new Contact (
-              name,
-              Request.Form["number"],
-              address
-            );
-          }
-          return View("Index");
-        }
-        
-        [HttpGet("/list/all")]
-        public ActionResult ListAll()
-        {
-          Dictionary<string, object> model = new Dictionary<string, object>();
-          model.Add("contacts", Contact.GetAll());
-          model.Add("msg", "Showing All Contacts");
-          return View("List", model);
-        }
-        
-        [HttpGet("/contact/{id}")]
-        public ActionResult Detail(int id)
-        {
-          return View("Detail", Contact.Find(id));
-        }
-        
-        [HttpGet("/new")]
-        public ActionResult ContactForm()
-        {
-          return View("ContactForm");
-        }
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View("Index");
     }
+    
+    [HttpPost("/add")]
+    public ActionResult AddNew()
+    {
+      string msg = "";
+      string name = Request.Form["name"];
+      if (name.Length > 0)
+      {
+        Address address = new Address(
+          Request.Form["address"],
+          Request.Form["city"],
+          Request.Form["state"],
+          Request.Form["zip"]
+        );
+        
+        Contact contact = new Contact (
+          name,
+          Request.Form["number"],
+          address
+        );
+        msg = "Contact info for '" + name + "' has been successfully added!";
+        return View("Confirm", msg);
+      }
+      msg = "Contact info could not be added!";
+      return View("Confirm", msg);
+    }
+    
+    [HttpGet("/list/all")]
+    public ActionResult ListAll()
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("contacts", Contact.GetAll());
+      model.Add("msg", "Showing All Contacts");
+      return View("List", model);
+    }
+    
+    [HttpGet("/contact/{id}")]
+    public ActionResult Detail(int id)
+    {
+      return View("Detail", Contact.Find(id));
+    }
+    
+    [HttpGet("/new")]
+    public ActionResult ContactForm()
+    {
+      return View("ContactForm");
+    }
+    
+    
+    
+    [HttpGet("/remove/{id}")]
+    public ActionResult Removeconfirm(int id)
+    {
+      string msg = "Contact info for '" + Contact.Find(id).GetName() + "' has been successfully removed!";
+      Contact.Remove(id);
+      return View("Confirm", msg);
+    }
+  }
 }
